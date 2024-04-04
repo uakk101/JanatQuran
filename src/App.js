b9dce83a7c7e16cce58803243c4bc822ca90c1df
Usman Ali <uakk101@gmail.com>
2024-04-04 16:32:10 +0500

    contact developer for help

--- src/App.js
+++ src/App.js
@@ -6,12 +6,14 @@ import Landing from "./screens/Landing.jsx";
 export default function App() {
   return (
     <>
-      <Helmet>
+
+      <h1>Contact Developer for help ....</h1>
+      {/* <Helmet>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link href="https://fonts.googleapis.com/css2?family=Khula:wght@400;600;800&display=swap" rel="stylesheet" />
       </Helmet>
-      <Landing />
+      <Landing /> */}
     </>
   );
 }
